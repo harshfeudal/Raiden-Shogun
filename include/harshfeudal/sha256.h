ace70ccf6b2e0caf158d376b8bd593fa60165310
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2022-11-03 11:01:28 +0700

    [Update]: Switch LICENSE

--- include/harshfeudal/sha256.h
+++ include/harshfeudal/sha256.h
@@ -1,15 +1,23 @@
-#pragma once
-
 /*
- * Copyright (c) - The Harshfeudal Projects
+ * Copyright 2022 harshfeudal and The Harshfeudal Projects contributors
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
  *
  * Encoder was written based on:
  * https://github.com/okdshin/PicoSHA2/blob/master/picosha2.h
  *
- */
-
-/*
  * The MIT License (MIT)
+ * 
  * Copyright (C) 2017 okdshin
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -27,6 +35,8 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
+#pragma once
 
 #ifndef PICOSHA2_BUFFER_SIZE_FOR_INPUT_ITERATOR
 #define PICOSHA2_BUFFER_SIZE_FOR_INPUT_ITERATOR \
