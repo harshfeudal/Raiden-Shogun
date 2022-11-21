ecc64de9be6aa7eac5561f8b086c5c4a12e708db
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2022-11-22 06:15:34 +0700

    [Update]: Add logger to check error

--- src/main.cpp
+++ src/main.cpp
@@ -88,6 +88,9 @@ int main()
 	// Console log prettier
 	SetConsoleTitle(TEXT("[BETA] v0.1.6.0 - Raiden Shogun Discord Bot - The Harshfeudal Projects"));
 
+	// Use this logger to check why the code is error (remove when done)
+	client.on_log(dpp::utility::cout_logger());
+
 	// Starting the bot
 	client.start(dpp::st_wait);
 	
